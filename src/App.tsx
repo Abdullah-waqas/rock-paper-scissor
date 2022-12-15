e99aa89ff75f9c08eecd9d34967c136b17806b10
unknown <dev.waqas08@gmail.com>
2022-12-15 21:21:14 +0100

    push basic structure of application

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import WelcomScreen from './WelcomeScreen/WelcomScreen';
+
+function App() {
+  return (
+    <WelcomScreen />
+  );
+}
+
+export default App;
---
+++ src/WelcomeScreen/WelcomScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function WelcomScreen() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Paper Scissor Stone Game</h1>
+      </header>
+      <button>Play</button>
+    </div>
+  );
+}
+
+export default WelcomScreen;
